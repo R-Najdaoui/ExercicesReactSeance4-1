84c3bef80f25fdfe9da4647eee9eb9c328c297f4
R-Najdaoui <najdaoui.ra@gmail.com>
2025-12-03 11:12:05 +0100

    Add exercice2 and update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import { useState } from "react";
 import "./App.css";
 import Exercice1 from "./components/Exercice1";
+import Exercice2 from "./components/Exercice2";
 
 function App() {
   const [output, setOutput] = useState(null);
 
   const exercises = {
-    1: () => <Exercice1 />
-  };
+  1: () => <Exercice1 />,
+  2: () => <Exercice2 />
+};
 
   return (
     <div style={{ padding: "20px" }}>
       <h2>Interface Exercices JSX</h2>
       <div>
-        <button
-          style={{ margin: "5px", padding: "10px 15px" }}
-          onClick={() => setOutput(exercises[1]())}
-        >
-          Exercice 1
-        </button>
-      </div>
+  {Array.from({ length: 2 }, (_, i) => (
+    <button
+      key={i}
+      style={{ margin: "5px", padding: "10px 15px" }}
+      onClick={() => setOutput(exercises[i + 1]())}
+    >
+      Exercice {i + 1}
+    </button>
+  ))}
+</div>
+
       <div
         style={{
           marginTop: "20px",
