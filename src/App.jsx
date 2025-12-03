39a2a1a5f4e5e9661a24f1cc0e2061047e87dcbd
R-Najdaoui <najdaoui.ra@gmail.com>
2025-12-03 11:07:46 +0100

    Add exercice 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Exercice1 from "./components/Exercice1";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [output, setOutput] = useState(null);
+
+  const exercises = {
+    1: () => <Exercice1 />
+  };
 
   return (
-    <>
+    <div style={{ padding: "20px" }}>
+      <h2>Interface Exercices JSX</h2>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button
+          style={{ margin: "5px", padding: "10px 15px" }}
+          onClick={() => setOutput(exercises[1]())}
+        >
+          Exercice 1
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div
+        style={{
+          marginTop: "20px",
+          padding: "10px",
+          border: "1px solid black",
+          minHeight: "100px",
+          width: "400px"
+        }}
+      >
+        {output}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Exercice1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Exercice1 = () => {
+  return <p>Hello React</p>;
+};
+
+export default Exercice1;
